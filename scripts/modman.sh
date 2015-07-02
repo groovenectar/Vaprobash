8c7424b11ac7e6e36bd268b88cc713ec9a6d65c4
Daniel Upshaw <contact@danielupshaw.com>
2015-07-02 13:50:28 -0400

    Update modman.sh

--- scripts/modman.sh
+++ scripts/modman.sh
@@ -4,5 +4,5 @@ echo ">>> Installing Modman"
 
 bash < <(wget -q --no-check-certificate -O - https://raw.github.com/groovenectar/modman/master/modman-installer)
 
-# May not be necessary for provisioning
-source ~/.profile
+# Should not be necessary for provisioning
+tty -s && source ~/.profile
