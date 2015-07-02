071ad4cd6b02602038b4836640526117bcd40cee
Daniel Upshaw <contact@danielupshaw.com>
2015-07-02 13:46:40 -0400

    Update modman.sh

--- scripts/modman.sh
+++ scripts/modman.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-echo ">>> Setting up Modman"
+echo ">>> Installing Modman"
 
 bash < <(wget -q --no-check-certificate -O - https://raw.github.com/groovenectar/modman/master/modman-installer)
 
+# May not be necessary for provisioning
 source ~/.profile
