735cb3d6b1ef6acf3d8081d1bd6c12d3368f70ca
Daniel Upshaw <contact@danielupshaw.com>
2017-01-16 16:46:51 -0500

    Update modman.sh

--- scripts/modman.sh
+++ scripts/modman.sh
@@ -2,7 +2,21 @@
 
 echo ">>> Installing Modman"
 
-bash < <(wget -q --no-check-certificate -O - https://raw.github.com/groovenectar/modman/master/modman-installer)
+SRC="https://raw.githubusercontent.com/colinmollenhour/modman/master/modman"
+DEST="/usr/bin/modman"
 
-# Should not be necessary for provisioning
-# . ~/.profile
+# test if curl/wget is installed
+if hash curl 2>&- ; then
+    CMD="sudo curl -s -L $SRC -o $DEST"
+elif hash wget 2>&- ; then
+    CMD="sudo wget -q --no-check-certificate -O $DEST $SRC"
+else
+   echo "You need to have curl or wget installed."
+   exit 1
+fi
+
+$CMD
+
+sudo chmod +x $DEST
+
+echo "Done. Modman installed in $HOME/bin/modman"
